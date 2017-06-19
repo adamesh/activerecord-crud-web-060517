ad75c0f6e1b5a7aa833df64a84a6f71254030119
adamesh <adameshh@gmail.com>
2017-06-19 08:08:04 -0400

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,24 @@
 class Movie < ActiveRecord::Base
+  def create_table
+    sql = <<-SQL
+      CREATE TABLE movies (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        release_date INTEGER,
+        director TEXT,
+        lead TEXT,
+        in_theaters BOOLEAN
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+
+  end
 end
+
+# columns for
+# title
+# release_date
+# director
+# lead
+# in_theaters
